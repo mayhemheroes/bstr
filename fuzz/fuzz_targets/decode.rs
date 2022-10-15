555e72e7e95eaa05dd02771ecd1ed6f767da9d6c
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-15 00:29:03 -0400

    import fixes

--- fuzz/fuzz_targets/decode.rs
+++ fuzz/fuzz_targets/decode.rs
@@ -3,5 +3,5 @@ use libfuzzer_sys::fuzz_target;
 use bstr::decode_utf8;
 
 fuzz_target!(|data: &[u8]| {
-    let (_, _) = decode_utf8(data);
+    let _ = decode_utf8(data);
 });
--- fuzz/fuzz_targets/decode_last.rs
+++ fuzz/fuzz_targets/decode_last.rs
@@ -1,7 +1,7 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use bstr::decode_utf8;
+use bstr::decode_last_utf8;
 
 fuzz_target!(|data: &[u8]| {
-    let (_, _) = decode_last_utf8(data);
+    let _ = decode_last_utf8(data);
 });
